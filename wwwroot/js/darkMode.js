74bc114dcb2b6ac51b29453a05e6c5c2d4ce3ebd
Thiago de Melo Lima <Thiago_badboy300@hotmail.com>
2020-05-23 09:51:20 -0300

    Initial commit

---
+++ wwwroot/js/darkMode.js
@@ -0,0 +1,22 @@
+﻿jQuery(".modoDark").hide();
+var xx = "corPreta";
+jQuery("#modoWhite").click(function () {
+    mudandoCor();
+})
+jQuery("#modoDark").click(function () {
+    mudandoCor();
+})
+function mudandoCor() {
+    if (xx == "corBranca") {
+        jQuery("#corpo").addClass("is-black");
+        xx = "corPreta";
+        jQuery(".modoDark").hide();
+        jQuery(".modoWhite").show();
+
+    } else {
+        jQuery("#corpo").removeClass("is-black");
+        xx = "corBranca";
+        jQuery(".modoDark").show();
+        jQuery(".modoWhite").hide();
+    }
+}
---
+++ wwwroot/js/modal.js
@@ -0,0 +1,15 @@
+﻿jQuery("#abrirModal").click(function () {
+    jQuery("#calcFreela").addClass("is-active");
+});
+jQuery("#closeCancel").click(function () {
+    fecharCalcFreela();
+
+});
+jQuery("#closeCanc").click(function () {
+    fecharCalcFreela();
+});
+
+function fecharCalcFreela() {
+    jQuery("#calcFreela")
+        .removeClass("is-active");
+}
